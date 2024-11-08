df00a8d1c86d352a2e0be6c51062889dd630a15f
Alexandre Macé <helloalexandremace@protonmail.com>
2024-11-08 18:26:03 +0100

    feat: update home UI according to design

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import {Button} from "@/components/ui/button";
 import {MoveUpRight} from "lucide-react";
+import Image from "next/image";
+import signature from "./signature.png"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,39 +32,59 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} font-mono antialiased bg-project-background font-medium p-4`}
       >
-      <div className={"grid grid-cols-3 mb-3"}>
-        <div className={"text-lg"}>GUILLAUME BERTHONNEAU</div>
-        <div className={"text-center text-lg"}>(GRID_LAB)</div>
-        <div className={"text-end text-muted-foreground"}>UPDATED ON NOVEMBER 12, 2024</div>
-      </div>
-      <div className={"flex gap-4 mb-20"}>
-          <div className={"flex flex-col gap-0"}>
-              <Button asChild variant={"link"}>
-                  <a href="https://google.com" target={"_blank"}>
-                      <MoveUpRight className={"h-1 w-1"}/>
-                      LINKEDIN
-                  </a>
-              </Button>
-              <Button asChild variant={"link"}>
-                  <a href="https://google.com" target={"_blank"}>
-                      <MoveUpRight className={"h-1 w-1"}/>
-                      X (TWITTER)
-                  </a>
-              </Button>
+      <div className={"grid grid-cols-3"}>
+          <div className={""}>
+              <div>
+                  GUILLAUME BERTHONNEAU
+              </div>
+              <div className={"flex gap-y-0 gap-x-4 mb-20"}>
+                  <div className={"flex items-start flex-col gap-0"}>
+                      <Button asChild variant={"link"} className={"px-0"}>
+                          <a href="https://google.com" target={"_blank"}>
+                              <MoveUpRight className={"h-1 w-1"}/>
+                              LINKEDIN
+                          </a>
+                      </Button>
+                      <Button asChild variant={"link"} className={"px-0"}>
+                          <a href="https://google.com" target={"_blank"}>
+                              <MoveUpRight className={"h-1 w-1"}/>
+                              X (TWITTER)
+                          </a>
+                      </Button>
+                  </div>
+                  <div className={"flex items-start flex-col gap-0"}>
+                      <Button asChild variant={"link"} className={"px-0"}>
+                          <a href="https://google.com" target={"_blank"}>
+                              <MoveUpRight className={"h-1 w-1"}/>
+                              READ.CV
+                          </a>
+                      </Button>
+                      <Button asChild variant={"link"} className={"px-0"}>
+                          <a href="https://google.com" target={"_blank"}>
+                              <MoveUpRight className={"h-1 w-1"}/>
+                              BEHANCE
+                          </a>
+                      </Button>
+                  </div>
+                  <div className={"flex items-start justify-end flex-col gap-0"}>
+                      <Button asChild variant={"link"} className={"px-0"}>
+                          <a href="https://google.com" target={"_blank"}>
+                              <MoveUpRight className={"h-1 w-1"}/>
+                              MAIL
+                          </a>
+                      </Button>
+                  </div>
+              </div>
           </div>
-          <div className={"flex flex-col gap-0"}>
-              <Button asChild variant={"link"}>
-                  <a href="https://google.com" target={"_blank"}>
-                      <MoveUpRight className={"h-1 w-1"}/>
-                      READ.CV
-                  </a>
-              </Button>
-              <Button asChild variant={"link"}>
-                  <a href="https://google.com" target={"_blank"}>
-                      <MoveUpRight className={"h-1 w-1"}/>
-                      BEHANCE
-                  </a>
-              </Button>
+          <div className={"relative text-center"}>
+              <div>I&#39;M A DESIGNER @ANAGRAM</div>
+              <Image src={signature} alt={"Signature"}
+                     className={"absolute left-1/2 -top-2 w-[182px] -translate-x-1/2"}/>
+          </div>
+          <div className={"text-end"}>
+              <div>(GRID_LAB)</div>
+              <div className={"text-sm text-muted-foreground"}>UPDATED ON NOVEMBER 12, 2024</div>
+              <div className={"text-sm"}>BORDEAUX, FRANCE</div>
           </div>
       </div>
       {children}
