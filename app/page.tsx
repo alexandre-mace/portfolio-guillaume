92cfedfd664716aa0509374377146befbe87811e
Alexandre Macé <helloalexandremace@protonmail.com>
2024-11-10 20:54:10 +0100

    feat(UI): update gap to make projects look bigger

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import getImageLink from "@/lib/getImageLink";
 import getVideoLink from "@/lib/getVideoLink";
 export default function Home() {
   return (
-    <div className={"container grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 mx-auto my-12 md:my-[180px] max-w-8xl gap-2 sm:gap-36 xl:gap-56"}>
+    <div className={"container grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 mx-auto my-12 md:my-[180px] max-w-8xl gap-2 sm:gap-16 xl:gap-24"}>
       {projects.map((project, index) => (
           <div key={index}>
             {project.image && !project.video &&
