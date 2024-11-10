8d2000cc73612c08387ebba2392f2c3b1be4e8f5
Alexandre Macé <helloalexandremace@protonmail.com>
2024-11-10 18:27:53 +0100

    feat(UI): update responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,44 +30,44 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} font-mono antialiased bg-project-background font-medium p-4`}
+        className={`${geistSans.variable} ${geistMono.variable} font-mono antialiased bg-project-background font-medium p-2 pt-10 md:p-4`}
       >
-      <div className={"grid grid-cols-3"}>
-          <div className={""}>
+      <div className={"grid md:grid-cols-3 gap-4"}>
+          <div className={"space-y-4 md:space-y-2 order-2 md:order-1"}>
               <div>
                   GUILLAUME BERTHONNEAU
               </div>
               <div className={"flex gap-y-0 gap-x-4 mb-20"}>
-                  <div className={"flex items-start flex-col gap-0"}>
-                      <Button asChild variant={"link"} className={"px-0"}>
+                  <div className={"flex items-start flex-col gap-4 md:gap-2"}>
+                      <Button asChild variant={"link"} className={"px-0 py-0 h-fit"}>
                           <a href="https://google.com" target={"_blank"}>
                               <MoveUpRight className={"h-1 w-1"}/>
                               LINKEDIN
                           </a>
                       </Button>
-                      <Button asChild variant={"link"} className={"px-0"}>
+                      <Button asChild variant={"link"} className={"px-0 py-0 h-fit"}>
                           <a href="https://google.com" target={"_blank"}>
                               <MoveUpRight className={"h-1 w-1"}/>
                               X (TWITTER)
                           </a>
                       </Button>
                   </div>
-                  <div className={"flex items-start flex-col gap-0"}>
-                      <Button asChild variant={"link"} className={"px-0"}>
+                  <div className={"flex items-start flex-col gap-4 md:gap-2"}>
+                      <Button asChild variant={"link"} className={"px-0 py-0 h-fit"}>
                           <a href="https://google.com" target={"_blank"}>
                               <MoveUpRight className={"h-1 w-1"}/>
                               READ.CV
                           </a>
                       </Button>
-                      <Button asChild variant={"link"} className={"px-0"}>
+                      <Button asChild variant={"link"} className={"px-0 py-0 h-fit"}>
                           <a href="https://google.com" target={"_blank"}>
                               <MoveUpRight className={"h-1 w-1"}/>
                               BEHANCE
                           </a>
                       </Button>
                   </div>
-                  <div className={"flex items-start justify-end flex-col gap-0"}>
-                      <Button asChild variant={"link"} className={"px-0"}>
+                  <div className={"flex items-start justify-end flex-col gap-4 md:gap-2"}>
+                      <Button asChild variant={"link"} className={"px-0 py-0 h-fit"}>
                           <a href="https://google.com" target={"_blank"}>
                               <MoveUpRight className={"h-1 w-1"}/>
                               MAIL
@@ -76,12 +76,12 @@ export default function RootLayout({
                   </div>
               </div>
           </div>
-          <div className={"relative text-center"}>
+          <div className={"relative md:text-center order-1 md:order-2"}>
               <div>I&#39;M A DESIGNER @ANAGRAM</div>
               <Image src={signature} alt={"Signature"}
-                     className={"absolute left-1/2 -top-2 w-[182px] -translate-x-1/2"}/>
+                     className={"absolute left-12 md:left-1/2 -top-10 w-[120px] md:w-[182px] md:-translate-x-1/2"}/>
           </div>
-          <div className={"text-end"}>
+          <div className={"md:text-end order-3"}>
               <div>(GRID_LAB)</div>
               <div className={"text-sm text-muted-foreground"}>UPDATED ON NOVEMBER 12, 2024</div>
               <div className={"text-sm"}>BORDEAUX, FRANCE</div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import projects from "@/data/projects.json"
 import Image from "next/image";
 export default function Home() {
   return (
-    <div className={"container grid md:grid-cols-4 mx-auto max-w-8xl gap-8 lg:gap-20 xl:gap-28 2xl:gap-36"}>
+    <div className={"container grid md:grid-cols-3 xl:grid-cols-4 mx-auto my-12 md:my-[180px] max-w-8xl gap-2 lg:gap-20 xl:gap-28 2xl:gap-36"}>
       {projects.map((project, index) => (
           <div key={index}>
             <Image width={300} height={250} className={"w-full h-auto"} src={project.image} alt={""}/>
